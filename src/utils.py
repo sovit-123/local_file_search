310ff961e9dd32c343a5df6b485a3db85724b612
sovit-123 <sovitrath5@gmail.com>
2024-12-13 10:41:04 +0530

    Added LLM streaming for CLI execution

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from transformers import TextStreamer
+
+YELLOW = "\033[93m"
+RESET = "\033[0m"
+
+class MyTextStreamer(TextStreamer):
+    """
+    This class is mainly used to override the methods for text streamer 
+    stdout and apply a different color for printing the text.
+    """
+    # Override method.
+    def on_finalized_text(self, text, stream_end = False):
+        """Prints the new text to stdout. If the stream is ending, also prints a newline."""
+        # print(text, flush=True, end="" if not stream_end else None)
+        print(f"{YELLOW}{text}{RESET}", flush=True, end="" if not stream_end else None)
