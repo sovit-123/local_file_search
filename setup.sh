885a34a6624b85d0aca57bb49ebfbc78d7092669
sovit-123 <sovitrath5@gmail.com>
2024-12-19 07:39:26 +0530

    Updated requirements

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
-pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio --index-url https://download.pytorch.org/whl/cu124
 pip install -r requirements.txt
